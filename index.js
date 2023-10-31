731418b99d71282ffe976f0ea2731cd949a8dfb4
DamascusKraken <DamascusKraken@gmail.com>
2023-10-31 01:16:53 -0500

    added question to obtain desired shape. Fixed formatting issues.

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
+const inquirer = require('inquirer');
+
+const questionShape  = [
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Select the shapeyou would like your Logo?',
+        choices: ['circle', 'square', 'triangle']
+    }
+];
+
+function getUserShape(){
+    inquirer.prompt(questionShape)
+    .then(function(userInput) 
+    {
+        if(userInput.shape == 'circle' )
+        {
+            createCircle
+        }
+    }
+)};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function shapes(fill, logoText, textColor){
     this.fill = fill;
     this.logoText = logoText;
     this.textColor = textColor;
 };
-
